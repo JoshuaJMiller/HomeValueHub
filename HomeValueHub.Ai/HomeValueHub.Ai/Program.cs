b8c40f0668b027f5ab909195ebed2662749dde0b
JoshuaJMiller <joshua.miller1014@gmail.com>
2023-06-22 20:04:53 -0700

    adds api and ai slns

---
+++ HomeValueHub.Ai/HomeValueHub.Ai/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.ML;
+
+namespace MLCA3
+{
+    internal class Program
+    {
+        static readonly string path = "C:\\Users\\joshu\\source\\repos\\Education\\C#\\ML\\InputData\\INPUTFILE.EXTENSION";
+
+        static void Main(string[] args)
+        {
+            // create context
+            var context = new MLContext(seed: 0);
+
+            // load data
+
+
+            // split the data into training and testing
+
+
+            // build and train the model
+
+
+            // evaluate the model
+
+
+            // use the model
+
+        }
+    }
+
+    internal class Input
+    {
+
+    }
+
+    internal class Output
+    {
+
+    }
+}
