d664fe851f62c9560ad0bc223fc93ef7cdf95f12
JoshuaJMiller <joshua.miller1014@gmail.com>
2023-06-24 17:01:12 -0700

    adds boiler plate pages, begins features form impl, adds shared sln

---
+++ HomeValueHub.Client/HomeValueHub.Client/Pages/Estimate.razor.cs
@@ -0,0 +1,21 @@
+﻿using HomeValueHub.Shared.Models;
+
+namespace HomeValueHub.Client.Pages
+{
+    public partial class Estimate
+    {
+        public HomeFeatures HomeFeatures { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            HomeFeatures = new HomeFeatures();
+        }
+
+        public async Task HandleValidSubmit()
+        {
+            await Console.Out.WriteLineAsync("Submitting! :)");
+        }
+    }
+}
